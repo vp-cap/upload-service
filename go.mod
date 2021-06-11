7ed95957e0ce79101c7b824fe577c859c62da48c
Omkar Prabhu <omkkarprabuinfinity@gmail.com>
2021-06-11 23:58:59 +0530

    dockerfile changes to make public

--- go.mod
+++ go.mod
@@ -8,7 +8,7 @@ require (
 	github.com/rs/cors v1.7.0
 	github.com/spf13/viper v1.3.2
 	github.com/streadway/amqp v1.0.0
-	github.com/vp-cap/data-lib v0.2.7
+	github.com/vp-cap/data-lib v0.2.9
 	google.golang.org/grpc v1.30.0
 	google.golang.org/protobuf v1.23.0
 )
--- start.sh
+++ start.sh
@@ -3,10 +3,10 @@ if [ "$1" = "bo" ]
 then
     echo ${GIT_USER}
     echo ${GIT_PASS}
-    docker build --build-arg GIT_USER=${GIT_USER} --build-arg GIT_PASS=${GIT_PASS} -t vp-cap/upload-service .
+     docker build -t vp-cap/upload-service .
 elif [ "$1" = "br" ]
 then
-    docker build --build-arg GIT_USER=${GIT_USER} --build-arg GIT_PASS=${GIT_PASS} -t vp-cap/upload-service .
+    docker build -t vp-cap/upload-service .
     docker stop upload-service && docker rm upload-service
     docker run --network=common --name upload-service -p 50051:50051 vp-cap/upload-service
 else
